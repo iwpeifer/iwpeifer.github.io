17257f5c2a012ec154b03baa903e9cc5d4730dc2
Isaac Peifer <iwpeifer@gmail.com>
2017-08-31 19:42:55 -0400

    made everything draggable lol

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
   $('#pic').hide()
+  $('#pic').draggable()
+  $('#content').draggable()
+  $('li').draggable()
+  $('p').draggable()
+  $('h2').draggable()
   displayAbout()
 })
 
@@ -17,16 +22,3 @@ function displayAbout(){
     }))
   })
 }
-
-$('.typewriter').typewrite({
-    'delay': 100, //time in ms between each letter
-    'extra_char': '|', //"cursor" character to append after each display
-    'trim': true, // Trim the string to type (Default: false, does not trim)
-    'callback': null // if exists, called after all effects have finished
-});
-//
-//     var msg = new SpeechSynthesisUtterance(`fuck u lol`);
-//   msg.voice = speechSynthesis.getVoices().filter(function(voice) { return voice.name == 'Friend'; })[0];
-//   speechSynthesis.speak(msg);
-//   })
-// }
